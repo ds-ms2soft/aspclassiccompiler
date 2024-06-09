eac7a6f1c0951614762833d891dcfa548bdbd6b8
Dave Sweeton <davesweeton@stoutsystems.com>
2024-06-09 14:21:57 -0400

    Renamed a file so it's not confused with the generated one.

---
+++ aspclassiccompiler/MvcTestApp/Includes/HeaderInfo_Mine.vb
@@ -0,0 +1,16 @@
+﻿Namespace Includes
+	Public Class HeaderInfo_Mine
+		Inherits IncludesBase
+	
+		Public Sub New(page As WebViewPage)
+			MyBase.New(page)
+			Raw("""<B>This text is coming from HeaderInfo.asp.
+
+</B>""")
+		End Sub
+
+		public Sub OutputDynamicText()
+			Raw("<p>Dynamic text:&nbsp;" & Now & "</p>")
+		End Sub
+	End Class
+End NameSpace
