6d1b48011df218d23238dbad7e2f854c41b1e2b4
DaveSweeton <ds@ms2soft.com>
2024-10-06 11:53:58 -0400

    Ignoring some unsupported files, and some TODOs and logging.

--- aspclassiccompiler/Transpiler/Program.cs
+++ aspclassiccompiler/Transpiler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Transpiler
 {
@@ -11,12 +12,37 @@ namespace Transpiler
 			new Program().TranspileAll();
 		}
 
+		private static Ms2Transpiler BuildService()
+		{
+			var service = new Ms2Transpiler();
+			service.IgnoreFile(new Regex("dynamic_array.asp$", RegexOptions.IgnoreCase))
+				.IgnoreFile(new Regex(@"Session\\MS2DotNetSession.asp", RegexOptions.IgnoreCase));
+			return service;
+		}
+		/*
+		 * TODO list:
+		 *
+		 * -Convert ADO commands to proxy objects
+		 * -security.vb /ref_path isn't properly being hoisted (asset_archive_delete.asp)");
+		 * -Handle some include files that are actually ASP, but don't have asp extensions (C:\source\TDMS\TCDS.Web\ajax\functions.asp.js)
+		 * -Some of those includes don't have ASP code, but do have HTML code (script tags) (C:\source\TDMS\TCDS.Web\ajax\functions_ttds.js) We could just transpile those to, or could convert them to normal JS script includes?
+		 */
 		[Test]
 		public void TranspileAll()
 		{
-			var service = new Ms2Transpiler();
+			var service = BuildService();
 
 			var errorCount = service.ParseAllFiles();
+
+			if (errorCount > 0)
+			{
+				Console.WriteLine("Errors found in files:");
+				foreach (var error in service.GetErrors())
+				{
+					Console.WriteLine(error);
+				}
+			}
+
 			service.IdentifyIncludes();
 
 			var count = service.TranspileValidPages();
@@ -27,7 +53,7 @@ namespace Transpiler
 		[Test]
 		public void OutputInvalid()
 		{
-			var service = new Ms2Transpiler();
+			var service = BuildService();
 
 			var errorCount = service.ParseAllFiles();
 			Console.WriteLine($"Invalid pages: {errorCount}");
